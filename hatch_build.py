4e759b8051f6f9312afc88401de362c113d54d9d
Leif Andersen <leif@leifandersen.pl>
2025-10-23 13:02:52 -0400

    Builds the jar, but still needs to copy it to src.

---
+++ hatch_build.py
@@ -0,0 +1,24 @@
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+import os
+import subprocess
+
+class CustomBuildHook(BuildHookInterface):
+    def initialize(self, version, build_data):
+        outPath = "src/gensql/gateway.jar"
+        inPath = "gateway"
+
+        # Check if Jar needs to be rebuilt
+        needsRebuild = False
+        if not os.path.exists(outPath):
+            needsRebuild = True
+        else:
+            outTime = os.path.getmtime(outPath)
+            for root, _, files in os.walk(inPath):
+                for file in files:
+                    if os.path.getmtime(os.path.join(root, file)) > outTime:
+                        needsRebuild = True
+                        break
+        
+        # Build Jar
+        if needsRebuild:
+            subprocess.run(["clojure", "-T:build", "uber"], check=True, cwd=inPath)
